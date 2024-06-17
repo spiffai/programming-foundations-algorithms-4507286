18cee9755c385f04040b565a0e2a3ac93cba9911
Alyssa Glasco <spiffai@gmail.com>
2024-06-17 22:40:09 +0000

    countdown

--- Start/Recursion/countdown_start.py
+++ Start/Recursion/countdown_start.py
@@ -3,7 +3,13 @@
 
 
 def countdown(x):
-    return
+    if x == 0:
+        print ("Done!")
+        return
+    else:
+        print(x, "...")
+        countdown(x-1)
+        print("hey!")
 
 
 countdown(5)
